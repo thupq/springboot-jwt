e52af794d7c6ec1c18b78930cdc012b0ffbef09c
thupq <phamquangthu2@gmail.com>
2023-03-21 00:00:43 +0700

    init project

---
+++ src/main/java/com/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.AppUser;
+
+public interface UserRepository extends JpaRepository<AppUser, Integer> {
+
+  boolean existsByUsername(String username);
+
+  AppUser findByUsername(String username);
+
+  @Transactional
+  void deleteByUsername(String username);
+
+}
